ce3b2884afbfceeaca4355ce384bf7de24a1f1e8
14dj <14dj69@gmail.com>
2024-06-09 01:35:05 +0900

    슬라이더 html, css

---
+++ Slider.js
@@ -0,0 +1,21 @@
+// slider.js
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+setInterval(nextSlide, 3000); // 3초마다 슬라이드 전환
